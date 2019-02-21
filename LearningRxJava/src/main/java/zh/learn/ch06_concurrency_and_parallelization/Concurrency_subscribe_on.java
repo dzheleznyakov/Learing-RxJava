fdf9e782befba91302cc0843eacf29a621766fe9
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2019-02-21 07:58:35 +0000

    Scheduler, understanding subscribeOn()

--- LearningRxJava/src/main/java/zh/learn/ch06_concurrency_and_parallelization/Concurrency_subscribe_on.java
+++ LearningRxJava/src/main/java/zh/learn/ch06_concurrency_and_parallelization/Concurrency_subscribe_on.java
@@ -6,12 +6,15 @@ import io.reactivex.schedulers.Schedulers;
 import zh.learn.Demonstration;
 import zh.learn.DisposableArray;
 
+import java.net.URL;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Concurrency_subscribe_on extends Demonstration {
     public static void main(String[] args) {
         demonstrate("subscribeOn()", Concurrency_subscribe_on::demonstrateSubscribeOn);
         demonstrate("Subscribe on the same computation thread", Concurrency_subscribe_on::subscribeOnTheSameThread);
+        demonstrate("Subsribe on a IO thread", Concurrency_subscribe_on::subscribeOnIOScheduler);
     }
 
     private static Disposable demonstrateSubscribeOn() {
@@ -42,5 +45,23 @@ public class Concurrency_subscribe_on extends Demonstration {
         sleep(10000);
 
         return DisposableArray.of(d1, d2);
+    }
+
+    private static Disposable subscribeOnIOScheduler() {
+        Disposable d = Observable.fromCallable(() -> getResponse("https://api.github.com/users/thomasnield"))
+                .subscribeOn(Schedulers.io())
+                .subscribe(System.out::println);
+
+        sleep(10000);
+
+        return d;
+    }
+
+    private static String getResponse(String path) {
+        try {
+            return new Scanner(new URL(path).openStream(), "UTF-8").useDelimiter("\\A").next();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
 }
