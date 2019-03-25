eedd4e0d5fff97517384a07a352686533a82ae81
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2019-03-25 16:36:24 +0000

    Custom operator for singles

--- LearningRxJava/src/main/java/zh/learn/ch09_transformers_and_customoperators/CustomOperatorForSingles.java
+++ LearningRxJava/src/main/java/zh/learn/ch09_transformers_and_customoperators/CustomOperatorForSingles.java
@@ -1,4 +1,26 @@
 package zh.learn.ch09_transformers_and_customoperators;
 
-public class CustomOperatorForSingles {
+import io.reactivex.Observable;
+import io.reactivex.SingleTransformer;
+import io.reactivex.disposables.Disposable;
+import zh.learn.Demonstration;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomOperatorForSingles extends Demonstration {
+    public static void main(String[] args) {
+        demonstrate("Transformer: to Single", CustomOperatorForSingles::transformToSingleton);
+    }
+
+    private static Disposable transformToSingleton() {
+        return Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
+                .toList()
+                .compose(toUnmodifiable())
+                .subscribe(System.out::println);
+    }
+
+    private static <T> SingleTransformer<Collection<T>, Collection<T>> toUnmodifiable() {
+        return singleObserver -> singleObserver.map(Collections::unmodifiableCollection);
+    }
 }
